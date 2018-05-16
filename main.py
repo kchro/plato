6a1b09198a8f7cc26e570dbf1c5f3eb16e5aa5f8
kchro <jhara18@stanford.edu>
2018-05-16 05:09:07 +0000

    baseline script

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 from keras.optimizers import *
 from keras.initializers import *
 from models.utils import NLVocab, FOLVocab
+from sklearn.model_selection import train_test_split
 
 BASELINE = True
 
@@ -22,25 +23,28 @@ def baseline():
     print source[:10]
     print target[:10]
     src_vocab = NLVocab(source)
-    src_inputs = src_vocab.preprocess(source[:10])
+    src_inputs = src_vocab.preprocess(source)
     tar_vocab = FOLVocab(target)
-    tar_inputs = tar_vocab.preprocess(target[:10])
+    tar_inputs = tar_vocab.preprocess(target)
+
+    X_train, X_test, y_train, y_test = train_test_split(src_inputs, tar_inputs, test_size=0.9)
 
     _, input_length, input_dim = src_inputs.shape
     _, output_length, output_dim = tar_inputs.shape
 
-    mod = Seq2Seq(output_dim=output_dim,
-                  hidden_dim=100,
-                  depth=2,
-                  output_length=output_length,
-                  input_shape=(input_length, input_dim))
+    mod = AttentionSeq2Seq(output_dim=output_dim,
+                           hidden_dim=100,
+                           depth=2,
+                           output_length=output_length,
+                           input_shape=(input_length, input_dim))
 
-    opt = SGD(lr=0.1)
-    mod.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-    history = mod.fit(src_inputs, tar_inputs, nb_epoch=1)
-    inputs = [src_vocab.sequence_to_text(seq) for seq in src_inputs[:5]]
-    gold = [tar_vocab.sequence_to_text(seq) for seq in tar_inputs[:5]]
-    predictions = mod.predict(src_inputs[:5])
+    #opt = SGD(lr=0.4)
+    opt = Adam()
+    mod.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
+    history = mod.fit(X_train, y_train, epochs=10)
+    inputs = [src_vocab.sequence_to_text(seq) for seq in X_test]
+    gold = [tar_vocab.sequence_to_text(seq) for seq in y_test]
+    predictions = mod.predict(X_test)
     pred = [tar_vocab.sequence_to_text(seq) for seq in predictions]
 
     for _input, _gold, _pred in zip(inputs, gold, pred):
@@ -51,4 +55,4 @@ def baseline():
 
 if __name__ == '__main__':
     if BASELINE:
-        baseline2()
+        baseline()
