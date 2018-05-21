0edf082028e3d0f57c4f12d78dd28e39a63ae2bb
Jeff Hara <jhara18@stanford.edu>
2018-05-20 22:12:29 -0700

    reduce epochs

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     print 'training the model...'
     history = model.train(X_train, y_train,
                           batch_size=1024,
-                          epochs=100)
+                          epochs=50)
     print 'done.'
 
     with open('logs/sessions/%s.json' % sess.replace(' ', '_'), 'w') as w:
