cb5104f7cf528f044b7e8fe788a406fe06a91e51
Jeff Hara <jhara18@stanford.edu>
2018-06-03 14:07:10 -0700

    cuda function

--- models/decoder.py
+++ models/decoder.py
@@ -33,15 +33,15 @@ class TreeDecoder(nn.Module):
                  device='cpu'):
         super(TreeDecoder, self).__init__()
         self.hidden_size = hidden_size
-        self.embed = nn.Embedding(output_size, hidden_size)
+        self.embed = nn.Embedding(output_size, hidden_size).cuda(device)
         # double the size of the LSTM (we append the parent <n> hidden state to input)
-        self.lstm = nn.LSTM(hidden_size*2, hidden_size, batch_first=True)
-        self.out = nn.Linear(hidden_size, output_size)
+        self.lstm = nn.LSTM(hidden_size*2, hidden_size, batch_first=True).cuda(device)
+        self.out = nn.Linear(hidden_size, output_size).cuda(device)
         self.softmax = nn.LogSoftmax(dim=1)
         self.device = device
 
-    def forward(self, input, hidden=None, parent=None, batch_size=1):
-        output = self.embed(input).view(batch_size, 1, -1)
+    def forward(self, input, hidden=None, parent=None):
+        output = self.embed(input).view(1, 1, -1)
         output = torch.cat([output, parent], dim=2)
         output = F.relu(output)
         output, hidden = self.lstm(output, hidden)
