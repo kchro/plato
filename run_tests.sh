a95883494954ed1dd826724e1eb3c10041341f11
Jeff Hara <jhara18@stanford.edu>
2018-06-03 13:08:09 -0700

    adding batch argument

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-python main.py --encoder seq --decoder seq --data med --epochs 100
+python main.py --encoder seq --decoder tree --data toy --epochs 10 --hidden 200 --batch 20
 #python main.py --encoder seq --decoder tree --data med --epochs 100
